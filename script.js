035520f9555048d5489926477632ced302317cf1
Rafael Uziel Cruz Garcia <rafaelcrugar2008@gmail.com>
2026-06-08 14:32:50 -0600

    pagina terminada.

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-
-    
-        function toggleMenu() {
+ function toggleMenu() {
             const navMenu = document.querySelector('.nav-menu');
             navMenu.classList.toggle('active');
         }
+
+
   
+
+
+        
